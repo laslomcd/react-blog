fc507a5e47acc0c49c20448826dcb0ad1044a90f
ryan <laslomcd@gmail.com>
2021-04-06 16:57:01 -0400

    started redux thunk

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import jsonplaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+	const response = await jsonplaceholder.get("/posts");
+	return {
+		type: "FETCH_POSTS",
+		payload: response,
+	};
+};
