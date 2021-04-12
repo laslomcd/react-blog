d26bdc1161ac19c90b44d19c3ecb052a1277c57e
ryan <laslomcd@gmail.com>
2021-04-12 16:59:48 -0400

    implemented redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,6 @@
 import jsonplaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonplaceholder.get("/posts");
-	return {
-		type: "FETCH_POSTS",
-		payload: response,
-	};
+	dispatch({ type: "FETCH_POSTS", payload: response });
 };
-// This wont work
