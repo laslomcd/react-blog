c91e7e111dff5b35c6d47822ba28ae4a757a672c
ryan <laslomcd@gmail.com>
2021-04-22 16:20:40 -0400

    work on actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,6 @@ export const fetchPosts = () => async (dispatch) => {
 };
 
 export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
 const _fetchUser = _.memoize(async (id, dispatch) => {
 	const response = await jsonplaceholder.get(`/users/${id}`);
 	dispatch({ type: "FETCH_USER", payload: response.data });
